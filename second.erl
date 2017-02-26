67562e06f1afbb2232d2c2ca8d2a42580aa95b21
Jessica White <code.jessw@gmail.com>
2017-02-26 11:04:44 +0000

    restructure files

--- second.erl
+++
@@ -1,9 +0,0 @@
--module(second).
--export([hypothenuse/2]).
-
-% The Pythagorean theory states the square of the length of the Hypotenuse
-% equals the sum of the squares of the other two sides.
-%https://en.wikipedia.org/wiki/Hypotenuse
-
-hypothenuse(W,H) ->
-    math:sqrt(first:square(W) + first:square(H)).
