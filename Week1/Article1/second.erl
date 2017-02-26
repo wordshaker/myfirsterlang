840979077d8eecf988c7b7fc7d6d5e3c86826ad2
Jessica White <code.jessw@gmail.com>
2017-02-26 16:40:10 +0000

    Add function for the area of a right angled triangle

--- Week1/Article1/second.erl
+++ Week1/Article1/second.erl
@@ -1,5 +1,5 @@
 -module(second).
--export([hypothenuse/2,perimeter/2]).
+-export([hypothenuse/2,perimeter/2,area/2]).
 
 % The Pythagorean theory states the square of the length of the Hypotenuse
 % equals the sum of the squares of the other two sides.
@@ -10,4 +10,9 @@ hypothenuse(W,H) ->
 % The Perimeter of a right angled triangle.
 
 perimeter(W,H) ->
-    second:hypothenuse(W,H)+W+H.
+second:hypothenuse(W,H)+W+H.
+
+% Area of a right angled triangle
+
+area(W,H) ->
+    (W*H)/2.
