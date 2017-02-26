3ab3ed48b54043d1ad131dc00311ed8d8a11f507
Jessica White <code.jessw@gmail.com>
2017-02-26 16:34:04 +0000

    Add second

---
+++ Week1/Article1/second.erl
@@ -0,0 +1,13 @@
+-module(second).
+-export([hypothenuse/2,perimeter/2]).
+
+% The Pythagorean theory states the square of the length of the Hypotenuse
+% equals the sum of the squares of the other two sides.
+
+hypothenuse(W,H) ->
+    math:sqrt(first:square(W) + first:square(H)).
+
+% The Perimeter of a right angled triangle.
+
+perimeter(W,H) ->
+    second:hypothenuse(W,H)+W+H.
