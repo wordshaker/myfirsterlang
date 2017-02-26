75e5f3d255115f53b1922e454bfed32c63b9d1c2
Jessica White <code.jessw@gmail.com>
2017-02-26 19:28:18 +0000

    XOR number 3

--- Week1/Article3/third.erl
+++ Week1/Article3/third.erl
@@ -1,10 +1,16 @@
 -module(third).
--export([xOr1/2,xOr2/2,maxThree/3,howManyEqual/3]).
+-export([xOr1/2,xOr2/2,xOr3/2,maxThree/3,howManyEqual/3]).
 
 % XOR
 
 xOr1(X,Y) ->
     X=/=Y.
+
+xOr2(X,Y) ->
+    not(X==Y).
+
+xOr3(X,Y) ->
+    not (X and Y).
 
 % Works out the maximum value of three integers.
 % Could assign the M = erlang:max(F,S) to a variable but I prefer this
