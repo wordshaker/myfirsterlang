d381fff4a9de45defb6375e161367b0b84f39286
Jessica White <code.jessw@gmail.com>
2017-02-27 21:01:46 +0000

    Fibonnaci Number based on the position in sequence worked out using head tail recursion

---
+++ Week1/TailRecursion/tailrecursion.erl
@@ -0,0 +1,21 @@
+-module(tailrecursion).
+-export([fib/1]).
+
+
+% Using Head Tail Recursion to find out the number of a number in the Fibonnaci Sequence based on 
+% position. Below is a manual working out of how it works for the fourth number in the sequence.
+
+% fib(4) = 3
+% fib(4,0,1) => fib(3, 1, 1) => fib(2, 1, 2) => 1 + 2 = 3 
+% fib(P,H,T) because P > 2 => fib(P,H,T) because P > 2 => H+T because P=<2
+
+fib(Position) ->
+    fib(Position, 0, 1).
+
+fib(0,_,_) ->
+    0;
+fib(Position,Head,Tail) when Position<3 ->
+    Head + Tail;
+fib(Position,Head,Tail) ->
+    fib(Position - 1, Tail, Head + Tail).
+    
