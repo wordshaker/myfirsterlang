a79f861962ffe7096285f080bc21a13e50c08e82
Jessica White <code.jessw@gmail.com>
2017-02-27 19:03:07 +0000

    Add recursive function for Fibonacci numbers

--- Week1/Article4/fourth.erl
+++ Week1/Article4/fourth.erl
@@ -1,9 +1,10 @@
 -module(fourth).
 -export([fib/1]).
 
+%Recursive function to calculate Fibonacci numbers
 fib(0) ->
 0;
 fib(1) -> 
 1;
-fib(Number) ->
+fib(Number) when N>1 ->
  fib(Number -1) + fib(Number -2).
