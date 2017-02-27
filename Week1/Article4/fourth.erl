962b7912fa2f9fa9c023c226b9aff28e92e17f32
Jessica White <code.jessw@gmail.com>
2017-02-27 19:22:45 +0000

    Add recursive function pieces of paper can be cut from N straight slices

--- Week1/Article4/fourth.erl
+++ Week1/Article4/fourth.erl
@@ -1,10 +1,18 @@
 -module(fourth).
--export([fib/1]).
+-export([fib/1,pieces/1]).
 
-%Recursive function to calculate Fibonacci numbers
+% Recursive function to calculate Fibonacci numbers
 fib(0) ->
 0;
 fib(1) -> 
 1;
-fib(Number) when N>1 ->
+fib(Number) when Number>1 ->
  fib(Number -1) + fib(Number -2).
+
+% How many pieces? -  tells you the maximum number of pieces into which you can cut a piece of paper with N straight line cuts.
+% http://mathworld.wolfram.com/CircleDivisionbyLines.html
+pieces(0) ->
+    1;
+pieces(N) when N>0 ->
+pieces(N - 1) + N.
+
