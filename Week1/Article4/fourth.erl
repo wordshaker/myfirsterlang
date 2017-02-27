10415f4d2a2826359425ac9afd7f33c8dd3a01f0
Jessica White <code.jessw@gmail.com>
2017-02-27 19:52:26 +0000

    Add recursive function pieces of any dimension shape can be cut from N straight slices

--- Week1/Article4/fourth.erl
+++ Week1/Article4/fourth.erl
@@ -1,5 +1,5 @@
 -module(fourth).
--export([fib/1,pieces/1,cubePieces/1]).
+-export([fib/1,pieces/1,cubePieces/1, dimensionPieces/2]).
 
 % Recursive function to calculate Fibonacci numbers
 fib(0) ->
@@ -14,10 +14,18 @@ fib(Number) when Number>1 ->
 pieces(0) ->
     1;
 pieces(N) when N>0 ->
-pieces(N - 1) + N.
+    pieces(N - 1) + N.
 
 % How many pieces? 3D
 cubePieces(0) ->
     1;
 cubePieces(N) when N>0 ->
-    cubePieces(N-1) + pieces(N - 1).
+    cubePieces(N - 1) + pieces(N - 1).
+
+% How many pieces? anyD
+dimensionPieces(_,0) ->
+    1;
+dimensionPieces(Dimensions,Slices) when Dimensions==2 ->
+    pieces(Slices);
+dimensionPieces(Dimensions,Slices) when Slices>0 ->
+    dimensionPieces(Dimensions, Slices - 1) + dimensionPieces(Dimensions - 1 ,Slices - 1).
