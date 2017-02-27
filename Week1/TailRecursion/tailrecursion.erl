b439f48f32d03c965847d67c63f17fcbc6da1522
Jessica White <code.jessw@gmail.com>
2017-02-27 21:34:38 +0000

    Use head tail recursion to work out if a number is a perfect number

--- Week1/TailRecursion/tailrecursion.erl
+++ Week1/TailRecursion/tailrecursion.erl
@@ -1,5 +1,5 @@
 -module(tailrecursion).
--export([fib/1]).
+-export([fib/1, perfect/1]).
 
 
 % Using Head Tail Recursion to find out the number of a number in the Fibonnaci Sequence based on 
@@ -20,3 +20,27 @@ fib(Position,Head,Tail) when Position<3 ->
 fib(Position,Head,Tail) ->
     fib(Position - 1, Tail, Head + Tail).
     
+% Perfect Numbers
+% Smallest perfect number is 6 so anything less == false
+% Work out total of numbers which are a modulo of given number. Half this. If equal to starting number its perfect.
+% https://en.wikipedia.org/wiki/Perfect_number
+% N  T  C
+% 6, 0, 6 => 
+% 6, 6, 5 =>
+% 6, 6, 4 =>
+% 6, 6, 3 =>
+% 6, 9, 2 =>
+% 6, 11,1 =>
+% 6, 12, 0 == true!
+
+perfect(Number) ->
+    perfect(Number,0,Number).
+
+perfect(Number,_,_) when Number<=5 ->
+    false;
+perfect(Number,Total,Count) when Count==0 ->
+    Total/2 == Number;
+perfect(Number,Total,Count) when Number rem Count>0->
+    perfect(Number, Total, Count -1);
+perfect(Number,Total,Count) when Number rem Count==0->
+    perfect(Number, Total + Count, Count -1).
