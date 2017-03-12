60171aa52c1ddf52eeaa6394adf1ec1ba7ab99e4
Jessica White <code.jessw@gmail.com>
2017-03-12 18:00:05 +0000

    Maximum and Product direct and tail recursion with test

--- Week2/Article2/functions_over_lists.erl
+++ Week2/Article2/functions_over_lists.erl
@@ -1,18 +1,41 @@
 -module(functions_over_lists).
--export([product/1,maximum/1]).
-
-product([]) ->
-    1;
-product([Number|Numbers]) -> 
-    Number * product(Numbers). 
+-export([product_direct/1,product_tail/1,maximum_tail/1,maximum_direct/1]).
 
 % The product of an empty list is 1 because the empty list is 1
 % https://en.wikipedia.org/wiki/Empty_product
 
-maximum([Number|Numbers]) ->
-    maximum([Number|Numbers], Number).
+product_direct([]) ->
+    1;
+product_direct([Number|Numbers]) -> 
+    Number * product_direct(Numbers). 
 
-maximum([], Maximum) ->
+product_tail([]) ->
+    1;
+product_tail([Number|Numbers]) -> 
+    product_tail([Number|Numbers], 1).  
+
+product_tail([Number|[]], Accumulator) -> 
+    Number*Accumulator;   
+product_tail([Number|Numbers], Accumulator) -> 
+    product_tail(Numbers, Number*Accumulator). 
+
+% For maximum I returned "NA" as you can't have a maximum of an empty list. 
+ 
+maximum_tail([]) ->
+    "NA";
+maximum_tail([Number|Numbers]) ->
+    maximum_tail([Number|Numbers], Number).
+
+maximum_tail([], Maximum) ->
     Maximum;
-maximum([Number|Numbers], Maximum) ->
-    maximum(Numbers, erlang:max(Number, Maximum)).
+maximum_tail([Number|Numbers], Maximum) ->
+    maximum_tail(Numbers, erlang:max(Number, Maximum)).
+
+maximum_direct([]) ->
+    "NA";
+maximum_direct([Number|[]]) -> 
+    Number;
+maximum_direct([X|Xs]) ->	
+    max(X, maximum_direct(Xs)).
+
+% Personally I get on best with tail recursion.
--- Week2/Article2/functions_over_lists_tests.erl
+++ Week2/Article2/functions_over_lists_tests.erl
@@ -3,8 +3,17 @@
 
 
 % Unit Tests For Product
-product_nil_test() -> 1 = functions_over_lists:product([]).
-product_one_test() -> 6 = functions_over_lists:product([1,2,3]).
+product_tail_nil_test() -> 1 = functions_over_lists:product_tail([]).
+product_tail_one_test() -> 6 = functions_over_lists:product_tail([1,2,3]).
 
-% Unit Tests for Maximum
-maximum_one_test() -> 7 = functions_over_lists:maximum([3,2,7,4]).
+% Unit Tests For Product
+product_direct_nil_test() -> 1 = functions_over_lists:product_direct([]).
+product_direct_one_test() -> 6 = functions_over_lists:product_direct([1,2,3]).
+
+% Unit Tests for Maximum Tail Recursion
+maximum_tail_nil_test() -> "NA" = functions_over_lists:maximum_tail([]).
+maximum_tail_one_test() -> 7 = functions_over_lists:maximum_tail([3,2,7,4]).
+
+% Unit Tests for Maximum Direct Recursion
+maximum_direct_nil_test() -> "NA" = functions_over_lists:maximum_direct([]).
+maximum_direct_one_test() -> 7 = functions_over_lists:maximum_direct([3,2,7,4]).
