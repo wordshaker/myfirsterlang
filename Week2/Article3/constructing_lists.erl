c6a890cccd4fec472af45d112c696478dfa35f97
Jessica White <code.jessw@gmail.com>
2017-03-14 21:25:08 +0000

    evens functions

--- Week2/Article3/constructing_lists.erl
+++ Week2/Article3/constructing_lists.erl
@@ -1,7 +1,17 @@
 -module(constructing_lists).
--export([double_direct/1]).
+-export([double_direct/1,evens/1]).
 
+% Doubles all the numbers in a list
 double_direct([]) ->
     [];
 double_direct([Number|Numbers]) -> 
-    [Number*2|double_direct(Numbers)].
+[Number*2|double_direct(Numbers)].
+
+
+% Removes odd numbers from a list
+evens([]) ->
+    [];
+evens([Number|Numbers]) when Number rem 2 == 0 ->
+    [Number|evens(Numbers)];
+evens([_Number|Numbers]) ->
+    evens(Numbers).
--- Week2/Article3/constructing_lists_tests.erl
+++ Week2/Article3/constructing_lists_tests.erl
@@ -5,3 +5,7 @@
 
 double_direct_nil_test() -> [] = constructing_lists:double_direct([]).
 double_direct_one_test() -> [2,4,6] = constructing_lists:double_direct([1,2,3]).
+
+evens_nil_test() -> [] = constructing_lists:evens([]).
+evens_one_test() -> [2,4,6] = constructing_lists:evens([1,2,3]).
+evens_two_test() -> [2,2,2] = constructing_lists:evens([1,2,3,2,2]).
