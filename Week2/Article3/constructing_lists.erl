9c788c30dfb811315dde5efde96e6a4131db5d2c
Jessica White <code.jessw@gmail.com>
2017-03-14 21:45:05 +0000

    Median functions

--- Week2/Article3/constructing_lists.erl
+++ Week2/Article3/constructing_lists.erl
@@ -1,5 +1,5 @@
 -module(constructing_lists).
--export([double_direct/1,evens/1]).
+-export([double_direct/1,evens/1,median/1]).
 
 % Doubles all the numbers in a list
 double_direct([]) ->
@@ -15,3 +15,14 @@ evens([Number|Numbers]) when Number rem 2 == 0 ->
     [Number|evens(Numbers)];
 evens([_Number|Numbers]) ->
     evens(Numbers).
+
+% median 
+median(Numbers) ->
+    OrderedList = lists:sort(Numbers),
+    Length = lists:flatlength(OrderedList),
+    Middle = Length div 2  + 1,
+    case Length rem 2 of 0 ->
+        (lists:nth(Middle,OrderedList) + lists:nth(Middle + 1, OrderedList)) /2;
+    _ ->
+        lists:nth(Middle, OrderedList)
+    end.
--- Week2/Article3/constructing_lists_tests.erl
+++ Week2/Article3/constructing_lists_tests.erl
@@ -9,3 +9,6 @@ double_direct_one_test() -> [2,4,6] = constructing_lists:double_direct([1,2,3]).
 evens_nil_test() -> [] = constructing_lists:evens([]).
 evens_one_test() -> [2,4,6] = constructing_lists:evens([1,2,3]).
 evens_two_test() -> [2,2,2] = constructing_lists:evens([1,2,3,2,2]).
+
+median_nil_test() -> [] = constructing_lists:median([]).
+median_one_test() -> 4 = constructing_lists:median([7,4,9,1,10,2,3]).
